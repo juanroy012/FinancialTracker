dbc615e886b7b9ab9626b2abc66f084111730510
Juan-Roy <juanroy012@gmail.com>
2026-02-18 23:57:10 -0500

    port changed from 8000 to 8080

--- backend/app/__main__.py
+++ backend/app/__main__.py
@@ -15,7 +15,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://financialtracker.fly.dev",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
